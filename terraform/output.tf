92d75ac7caf8b316b49029b2b15b9ccd8c9d9c07
johnsamey <johnsamyut@gmail.com>
2024-09-30 06:25:33 +0300

    add output.tf file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
 resource "local_file" "inventory" {
   content = <<EOF
 [app]
-${join("\n", [for instance in aws_instance.Public_EC2 : instance.public_ip if instance.public_ip != null])}
+${join("\n", [for instance in aws_instance.nexus_instance : instance.public_ip if instance.public_ip != null])}
 EOF
   filename = "inventory.txt"
 }
